c3f6502c8c14e4cefd80ac9f6ccf8b83915dab82
Samet Kabakçı <sametkabakci@Samet-Dizustu-Bilgisayar.local>
2024-09-19 19:04:47 +0300

    install react

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-800">Main</h1>
+        </div>
+    );
+}
+
+export default Main
